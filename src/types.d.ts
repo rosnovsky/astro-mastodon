141d2190f0a80aeff0d9c8cefd2db915cd0b80a9
Art Rosnovsky <art@rosnovsky.us>
2024-02-28 13:39:39 -0800

    chore: tweak media and link attachments

--- src/types.d.ts
+++ src/types.d.ts
@@ -87,19 +87,19 @@ interface MastodonAccount {
 // This appears to be a "oEmbed" kind of card for YouTube and other media.
 interface MediaCard {
   url: string;
-  title: string;
-  description: string;
-  image: string;
+  title?: string;
+  description?: string;
+  image?: string;
   type: string;
-  author_name: string;
-  author_url: string;
-  provider_name: string;
-  provider_url: string;
-  html: string;
-  width: number;
-  height: number;
-  embed_url: string;
-  blurhash: string;
+  author_name?: string;
+  author_url?: string;
+  provider_name?: string;
+  provider_url?: string;
+  html?: string;
+  width?: number;
+  height?: number;
+  embed_url?: string;
+  blurhash?: string;
 }
 
 export interface MastodonPost {
